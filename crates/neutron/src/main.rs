45f684744da72e83ad1d3bb962f7d84eccc9ccca
John Lewis <github@jlewis.sh>
2024-12-04 16:09:27 +0100

    feat: basic iced app

--- crates/neutron/src/main.rs
+++ crates/neutron/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{
+  Theme,
+  widget::{Column, column, text},
+};
+
+const APP_TITLE: &str = "Neutron";
+
+#[derive(Debug, Clone, Copy)]
+pub enum AppMessage {}
+
+#[derive(Debug, Default)]
+struct App {
+  value: i32,
 }
+
+impl App {
+  pub fn view(&self) -> Column<AppMessage> {
+    column![text(self.value).size(50)]
+  }
+
+  pub fn update(&mut self, message: AppMessage) { match message {} }
+
+  pub fn theme(&self) -> Theme { Theme::TokyoNight }
+
+  pub fn run() -> iced::Result {
+    iced::application(APP_TITLE, Self::update, Self::view)
+      .theme(Self::theme)
+      .run()
+  }
+}
+
+fn main() -> iced::Result { App::run() }
