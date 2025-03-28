3bf6937c09566210dc3ccde924d5a6f69993e525
John Lewis <github@jlewis.sh>
2025-03-28 14:49:54 +0100

    feat: dump

---
+++ crates/neutron/src/colors.rs
@@ -0,0 +1,74 @@
+#![allow(dead_code)]
+
+use radix_colors_rs::*;
+
+macro_rules! radix_to_ratatui_const {
+  ($radix_color:ident, $ratatui_color:ident) => {
+    pub const $ratatui_color: ratatui::style::Color =
+      ratatui::style::Color::Rgb(
+        $radix_color.r,
+        $radix_color.g,
+        $radix_color.b,
+      );
+  };
+}
+
+// radix_to_ratatui_const!(SAGEDARK_SAGE1, BASE_1_RATATUI);
+// radix_to_ratatui_const!(SAGEDARK_SAGE2, BASE_2_RATATUI);
+// radix_to_ratatui_const!(SAGEDARK_SAGE3, BASE_3_RATATUI);
+// radix_to_ratatui_const!(SAGEDARK_SAGE4, BASE_4_RATATUI);
+// radix_to_ratatui_const!(SAGEDARK_SAGE5, BASE_5_RATATUI);
+// radix_to_ratatui_const!(SAGEDARK_SAGE6, BASE_6_RATATUI);
+// radix_to_ratatui_const!(SAGEDARK_SAGE7, BASE_7_RATATUI);
+// radix_to_ratatui_const!(SAGEDARK_SAGE8, BASE_8_RATATUI);
+// radix_to_ratatui_const!(SAGEDARK_SAGE9, BASE_9_RATATUI);
+// radix_to_ratatui_const!(SAGEDARK_SAGE10, BASE_10_RATATUI);
+// radix_to_ratatui_const!(SAGEDARK_SAGE11, BASE_11_RATATUI);
+// radix_to_ratatui_const!(SAGEDARK_SAGE12, BASE_12_RATATUI);
+// radix_to_ratatui_const!(GRASSDARK_GRASS1, PRIMARY_1_RATATUI);
+// radix_to_ratatui_const!(GRASSDARK_GRASS2, PRIMARY_2_RATATUI);
+// radix_to_ratatui_const!(GRASSDARK_GRASS3, PRIMARY_3_RATATUI);
+// radix_to_ratatui_const!(GRASSDARK_GRASS4, PRIMARY_4_RATATUI);
+// radix_to_ratatui_const!(GRASSDARK_GRASS5, PRIMARY_5_RATATUI);
+// radix_to_ratatui_const!(GRASSDARK_GRASS6, PRIMARY_6_RATATUI);
+// radix_to_ratatui_const!(GRASSDARK_GRASS7, PRIMARY_7_RATATUI);
+// radix_to_ratatui_const!(GRASSDARK_GRASS8, PRIMARY_8_RATATUI);
+// radix_to_ratatui_const!(GRASSDARK_GRASS9, PRIMARY_9_RATATUI);
+// radix_to_ratatui_const!(GRASSDARK_GRASS10, PRIMARY_10_RATATUI);
+// radix_to_ratatui_const!(GRASSDARK_GRASS11, PRIMARY_11_RATATUI);
+// radix_to_ratatui_const!(GRASSDARK_GRASS12, PRIMARY_12_RATATUI);
+
+radix_to_ratatui_const!(SANDDARK_SAND1, BASE_1_RATATUI);
+radix_to_ratatui_const!(SANDDARK_SAND2, BASE_2_RATATUI);
+radix_to_ratatui_const!(SANDDARK_SAND3, BASE_3_RATATUI);
+radix_to_ratatui_const!(SANDDARK_SAND4, BASE_4_RATATUI);
+radix_to_ratatui_const!(SANDDARK_SAND5, BASE_5_RATATUI);
+radix_to_ratatui_const!(SANDDARK_SAND6, BASE_6_RATATUI);
+radix_to_ratatui_const!(SANDDARK_SAND7, BASE_7_RATATUI);
+radix_to_ratatui_const!(SANDDARK_SAND8, BASE_8_RATATUI);
+radix_to_ratatui_const!(SANDDARK_SAND9, BASE_9_RATATUI);
+radix_to_ratatui_const!(SANDDARK_SAND10, BASE_10_RATATUI);
+radix_to_ratatui_const!(SANDDARK_SAND11, BASE_11_RATATUI);
+radix_to_ratatui_const!(SANDDARK_SAND12, BASE_12_RATATUI);
+radix_to_ratatui_const!(ORANGEDARK_ORANGE1, PRIMARY_1_RATATUI);
+radix_to_ratatui_const!(ORANGEDARK_ORANGE2, PRIMARY_2_RATATUI);
+radix_to_ratatui_const!(ORANGEDARK_ORANGE3, PRIMARY_3_RATATUI);
+radix_to_ratatui_const!(ORANGEDARK_ORANGE4, PRIMARY_4_RATATUI);
+radix_to_ratatui_const!(ORANGEDARK_ORANGE5, PRIMARY_5_RATATUI);
+radix_to_ratatui_const!(ORANGEDARK_ORANGE6, PRIMARY_6_RATATUI);
+radix_to_ratatui_const!(ORANGEDARK_ORANGE7, PRIMARY_7_RATATUI);
+radix_to_ratatui_const!(ORANGEDARK_ORANGE8, PRIMARY_8_RATATUI);
+radix_to_ratatui_const!(ORANGEDARK_ORANGE9, PRIMARY_9_RATATUI);
+radix_to_ratatui_const!(ORANGEDARK_ORANGE10, PRIMARY_10_RATATUI);
+radix_to_ratatui_const!(ORANGEDARK_ORANGE11, PRIMARY_11_RATATUI);
+radix_to_ratatui_const!(ORANGEDARK_ORANGE12, PRIMARY_12_RATATUI);
+
+pub use BASE_1_RATATUI as BASE_COLOR_RATATUI;
+pub use BASE_2_RATATUI as BACKGROUND_COLOR_RATATUI;
+pub use BASE_7_RATATUI as NORMAL_BORDER_COLOR_RATATUI;
+pub use BASE_10_RATATUI as LINEART_COLOR_RATATUI;
+pub use BASE_11_RATATUI as DIM_TEXT_COLOR_RATATUI;
+pub use BASE_12_RATATUI as NORMAL_TEXT_COLOR_RATATUI;
+pub use PRIMARY_7_RATATUI as ACTIVE_BORDER_COLOR_RATATUI;
+pub use PRIMARY_9_RATATUI as PUNCHY_TEXT_COLOR_RATATUI;
+pub use PRIMARY_11_RATATUI as TITLE_COLOR_RATATUI;
--- crates/neutron/src/main.rs
+++ crates/neutron/src/main.rs
@@ -1,3 +1,27 @@
+mod colors;
+
+use std::{sync::Arc, time::Duration};
+
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use futures::StreamExt;
+use ratatui::{
+  Frame,
+  style::Stylize,
+  text::{Line, Text},
+  widgets::{Block, Paragraph},
+};
+use tokio::{
+  sync::{
+    Mutex,
+    mpsc::{self},
+  },
+  time::interval,
+};
+
+use self::colors::{BACKGROUND_COLOR_RATATUI, NORMAL_TEXT_COLOR_RATATUI};
+
+const FRAME_DURATION: Duration = Duration::from_nanos(1_000_000_000 / 60);
+
 struct ItemId(ulid::Ulid);
 
 struct Item {
@@ -9,4 +33,127 @@ enum DependencySet {
   All(Vec<ItemId>),
 }
 
-fn main() {}
+#[derive(Clone)]
+struct AppState {
+  shutdown: bool,
+  counter:  usize,
+}
+
+enum Command {
+  Exit,
+  IncrementCounter(usize),
+}
+
+#[allow(clippy::derivable_impls)]
+impl Default for AppState {
+  fn default() -> Self {
+    AppState {
+      shutdown: false,
+      counter:  0,
+    }
+  }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), ()> {
+  let state = Arc::new(Mutex::new(AppState::default()));
+
+  let (commands, command_rx) = mpsc::channel(100);
+
+  tokio::select! {
+    _ = tokio::spawn(draw_task(state.clone())) => {},
+    _ = tokio::spawn(command_runner(state.clone(), command_rx)) => {},
+    _ = tokio::spawn(event_handler(commands.clone())) => {},
+    _ = tokio::spawn(shutdown_task(state.clone())) => {},
+  };
+
+  ratatui::restore();
+
+  Ok(())
+}
+
+async fn shutdown_task(state: Arc<Mutex<AppState>>) {
+  let mut interval = interval(FRAME_DURATION);
+
+  loop {
+    interval.tick().await;
+    let state = state.lock().await;
+    if state.shutdown {
+      break;
+    }
+  }
+}
+
+async fn event_handler(commands: mpsc::Sender<Command>) {
+  let mut event_stream = crossterm::event::EventStream::new();
+
+  loop {
+    if let Some(Ok(event)) = event_stream.next().await {
+      let _ = match event {
+        Event::Key(KeyEvent {
+          code: KeyCode::Char('q') | KeyCode::Esc,
+          modifiers: KeyModifiers::NONE,
+          kind: KeyEventKind::Press,
+          ..
+        }) => commands.send(Command::Exit).await,
+        Event::Key(KeyEvent {
+          code: KeyCode::Char(' '),
+          modifiers: KeyModifiers::NONE,
+          kind: KeyEventKind::Press,
+          ..
+        }) => commands.send(Command::IncrementCounter(1)).await,
+        _ => Ok(()),
+      };
+    }
+  }
+}
+
+async fn command_runner(
+  state: Arc<Mutex<AppState>>,
+  mut command_rx: mpsc::Receiver<Command>,
+) {
+  loop {
+    let Some(command) = command_rx.recv().await else {
+      break;
+    };
+
+    let mut lock = state.lock().await;
+    match command {
+      Command::Exit => lock.shutdown = true,
+      Command::IncrementCounter(amount) => lock.counter += amount,
+    }
+  }
+}
+
+async fn draw_task(state: Arc<Mutex<AppState>>) {
+  let mut terminal = ratatui::init();
+  let mut interval = interval(FRAME_DURATION);
+
+  loop {
+    interval.tick().await;
+
+    let state_snapshot = {
+      let state = state.lock().await;
+      AppState::clone(&state)
+    };
+
+    terminal
+      .draw(draw(state_snapshot))
+      .expect("failed to draw frame");
+  }
+}
+
+fn draw(state: AppState) -> impl FnOnce(&mut Frame) {
+  move |frame: &mut Frame| {
+    let block = Block::new()
+      .fg(NORMAL_TEXT_COLOR_RATATUI)
+      .bg(BACKGROUND_COLOR_RATATUI);
+    let par = Paragraph::new(Text::from(vec![
+      Line::from("Hello World!"),
+      Line::from(format!("Counter: {}", state.counter)),
+    ]))
+    .block(block);
+
+    frame.render_widget(par, frame.area());
+  }
+}
